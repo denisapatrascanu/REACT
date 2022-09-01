a2141fc3e73cfe540a25007d36428fd6740b3932
Statia 76 <statia76@laurian.domain>
2022-09-01 12:06:39 +0300

    Added Prismic to project

--- next-in-atl/components/Song/Song.tsx
+++ next-in-atl/components/Song/Song.tsx
@@ -1,35 +1,35 @@
-import styles  from "./Song.module.scss"
+import styles from "./Song.module.scss"
 
-export interface ISongInfo{
-    imageLink:string;
-    title:string;
-    album:string;
-    duration:string;
+export interface ISongInfo {
+    imageLink: string;
+    title: string;
+    album: string;
+    duration: string;
 }
 interface IProps {
     index: number;
     songInfo: ISongInfo;
 }
-export const Song = (props : IProps) => {
+export const Song = (props: IProps) => {
     const index = props.index;
     const songInfo = props.songInfo;
     return (
         <div className={styles["melody"]}>
-                        <span className={styles["song-nr"]}>
-                            {index}
-                        </span>
-                        <img src={songInfo.imageLink} className={styles["song-cover"]}>
-                        </img>
-                        <span className={styles["song-title"]}>
-                            {songInfo.title}
-                        </span>
-                        <span className={styles["song-album"]}>
-                            {songInfo.album}
-                        </span>
-                        <span className={styles["song-duration"]}>
-                            {songInfo.duration}
-                        </span>
-                    </div>
+            <span className={styles["song-nr"]}>
+                {index}
+            </span>
+            <img src={songInfo.imageLink} className={styles["song-cover"]}>
+            </img>
+            <span className={styles["song-title"]}>
+                {songInfo.title}
+            </span>
+            <span className={styles["song-album"]}>
+                {songInfo.album}
+            </span>
+            <span className={styles["song-duration"]}>
+                {songInfo.duration}
+            </span>
+        </div>
 
 
     );
